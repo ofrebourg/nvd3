edc153e78100767da6f5f687c054c15c646f2319
Oli Frebourg <ofrebour@cisco.com>
2017-05-17 21:55:05 +0100

    Updated version to 1.8.6

--- package.js
+++ package.js
@@ -5,8 +5,8 @@
 Package.describe({
     "name": 'nvd3:nvd3',
     summary: 'Nvd3.org charts.',
-    version: '1.8.5-dev',
-    git: "https://github.com/novus/nvd3.git"
+    version: '1.8.6',
+    git: "https://github.com/ofrebourg/nvd3.git"
 });
 Package.on_use(function (api) {
     api.versionsFrom("METEOR@1.0");
